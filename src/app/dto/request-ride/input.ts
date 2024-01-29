7cc6ea6778cdf734a35a5e1142c9d41b2414c386
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-01-28 22:56:24 -0300

    feat RequestRideUseCase dtos

---
+++ src/app/dto/request-ride/input.ts
@@ -0,0 +1,7 @@
+export default interface InputRequestRide {
+  passengerId: string;
+  fromLat: number;
+  fromLong: number;
+  toLat: number;
+  toLong: number;
+};
---
+++ src/app/dto/request-ride/output.ts
@@ -0,0 +1,3 @@
+export default interface OutputRequestRide {
+  rideId: string;
+};
