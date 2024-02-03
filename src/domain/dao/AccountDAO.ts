93b84f5d3e737069b9fe5ddd4d5f5977437857aa
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-02-03 15:48:11 -0300

    remove: arquivos desnecessarios e pastas

--- src/domain/dao/AccountDAO.ts
+++
@@ -1,8 +0,0 @@
-import Account from '../entity/Account';
-
-export default interface AccountDAO {
-  findAccountByEmail(email: string): Promise<Account | null>;
-  findAccountById(accountId: string): Promise<Account | null>;
-  createPassengerAccount(name: string, email: string, cpf: string): Promise<Account>;
-  createDriverAccount(name: string, email: string, cpf: string, carPlate: string | null): Promise<Account>;
-}
