ab0461f4a27c92194a129bd9a6167c34f744552e
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-02-04 22:13:39 -0300

    feat: adicionado camada incial para isntanciação de objetos

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import app from './infra/web/express/main';
+import ExpressAdapter from './infra/http/ExpressAdapter';
+import MainController from './infra/controller/mainController';
+import Signup from './app/usecase/Signup';
+import MemoryAccountRepository from './infra/repository/MemoryAccountRepository';
+import GetAccount from './app/usecase/GetAccount';
 
-app.listen(3000, () => {
+const server = new ExpressAdapter();
+
+const accountRepository = new MemoryAccountRepository();
+const singup = new Signup(accountRepository);
+const getAccount = new GetAccount(accountRepository);
+const controller = new MainController(server, singup, getAccount);
+
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
