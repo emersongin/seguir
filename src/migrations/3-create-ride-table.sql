fb04aba22b00cfaa86342a542e0c01eb471b5ccd
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-02-08 22:34:56 -0300

    feat: alteração emmigrations

--- src/migrations/3-create-ride-table.sql
+++ src/migrations/3-create-ride-table.sql
@@ -1,13 +1,15 @@
 create table ride (
-  ride_id uuid,
-  passenger_id uuid,
-  driver_id uuid,
-  status text,
-  fare numeric,
-  distance numeric,
-  from_lat numeric,
-  from_long numeric,
-  to_lat numeric,
-  to_long numeric,
-  date timestamp
+	ride_id uuid primary key,
+	passenger_id uuid,
+	driver_id uuid,
+	fare numeric,
+	distance numeric,
+	status text,
+	from_lat numeric,
+	from_long numeric,
+	to_lat numeric,
+	to_long numeric,
+	last_lat numeric,
+	last_long numeric,
+	date timestamp
 );
--- src/migrations/4-create-position-table.sql
+++ src/migrations/4-create-position-table.sql
@@ -1,7 +1,7 @@
 create table position (
-  position_id uuid,
-  ride_id uuid,
-  lat numeric,
-  long numeric,
-  date timestamp
+	position_id uuid primary key,
+	ride_id uuid,
+	lat numeric,
+	long numeric,
+	date timestamp
 );
