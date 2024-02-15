35292ec5cf5a1b2a671ef4977187f753f9fe624e
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-02-15 19:04:04 -0300

    test: casos de testes para FinishRide

--- test/integration/app/usecase/FinishRide.test.ts
+++ test/integration/app/usecase/FinishRide.test.ts
@@ -7,10 +7,14 @@ import AccountRepositoryDatabase from '../../../../src/infra/repository/AccountR
 import RideRepositoryDatabase from '../../../../src/infra/repository/RideRepositoryDatabase';
 import Account from '../../../../src/domain/entity/Account';
 import Ride from '../../../../src/domain/entity/Ride';
+import PositionRepository from '../../../../src/infra/repository/PositionRepository';
+import PositionRepositoryDatabase from '../../../../src/infra/repository/PositionRepositoryDatabase';
+import Position from '../../../../src/domain/entity/Position';
 
 describe('testes para caso de uso de finalização de corrida', () => {
   let accountRepository: AccountRepository;
   let rideRepository: RideRepository;
+  let positionRepository: PositionRepository;
   let useCase: FinishRide;
   let rideData: {
     rideId: string;
@@ -58,7 +62,8 @@ describe('testes para caso de uso de finalização de corrida', () => {
       -46.62543
     ));
     if (!rideRequested) throw new Error('Ride not found');
-    useCase = new FinishRide(rideRepository);
+    positionRepository = new PositionRepositoryDatabase(database);
+    useCase = new FinishRide(rideRepository, positionRepository);
     rideData = {
       rideId: rideRequested.id,
       driverId: driverAccount.id,
@@ -70,8 +75,64 @@ describe('testes para caso de uso de finalização de corrida', () => {
     const ride = await rideRepository.getById(rideId);
     if (!ride) throw new Error('ride not found');
     ride.acceptRide(driverId);
-    rideRepository.update(ride);
+    await rideRepository.update(ride);
     const input = { rideId };
     await expect(() => useCase.execute(input)).rejects.toThrowError('Ride is not in progress.');
+  });
+
+  it('deve calcular a distância percorrida em km', async () => {
+    const { rideId, driverId } = rideData;
+    const ride = await rideRepository.getById(rideId);
+    if (!ride) throw new Error('ride not found');
+    ride.acceptRide(driverId);
+    ride.startRide();
+    await rideRepository.update(ride);
+    await positionRepository.save(Position.create(rideId, -23.56168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.57168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.58168, -46.62543));
+    const input = { rideId };
+    const output = await useCase.execute(input);
+    expect(output).toBeUndefined();
+    const expectDistanceTotal = 2;
+    const rideFinished = await rideRepository.getById(rideId);
+    if (!rideFinished) throw new Error('ride not found');
+    expect(rideFinished.getDistance()).toBe(expectDistanceTotal);
+  });
+
+  it('deve calcular o valor da corrida', async () => {
+    const { rideId, driverId } = rideData;
+    const ride = await rideRepository.getById(rideId);
+    if (!ride) throw new Error('ride not found');
+    ride.acceptRide(driverId);
+    ride.startRide();
+    await rideRepository.update(ride);
+    await positionRepository.save(Position.create(rideId, -23.56168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.57168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.58168, -46.62543));
+    const input = { rideId };
+    const output = await useCase.execute(input);
+    expect(output).toBeUndefined();
+    const expectFareTotal = 2 * 2.1;
+    const rideFinished = await rideRepository.getById(rideId);
+    if (!rideFinished) throw new Error('ride not found');
+    expect(rideFinished.getFare()).toBe(expectFareTotal);
+  });
+
+  it('deve finalizar a corrida com status de completed', async () => {
+    const { rideId, driverId } = rideData;
+    const ride = await rideRepository.getById(rideId);
+    if (!ride) throw new Error('ride not found');
+    ride.acceptRide(driverId);
+    ride.startRide();
+    await rideRepository.update(ride);
+    await positionRepository.save(Position.create(rideId, -23.56168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.57168, -46.62543));
+    await positionRepository.save(Position.create(rideId, -23.58168, -46.62543));
+    const input = { rideId };
+    const output = await useCase.execute(input);
+    expect(output).toBeUndefined();
+    const rideFinished = await rideRepository.getById(rideId);
+    if (!rideFinished) throw new Error('ride not found');
+    expect(rideFinished.getStatus()).toBe('completed');
   });
 });
