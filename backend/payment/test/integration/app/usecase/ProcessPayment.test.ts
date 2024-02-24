4f19371ac9b2be1ceb85d86ba21b1af9bcf6b8bf
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-02-24 10:15:40 -0300

    test: ajuste importação de ProcessPayment

--- backend/payment/test/integration/app/usecase/ProcessPayment.test.ts
+++ backend/payment/test/integration/app/usecase/ProcessPayment.test.ts
@@ -1,11 +1,11 @@
-import ProcessPayment from '../../../src/app/usecase/ProcessPayment';
-import PaymentGateway from '../../../src/infra/gateway/PaymentGateway';
-import PaymentGatewayRede from '../../../src/infra/gateway/PaymentGatewayRede';
-import TransactionRepository from '../../../src/infra/repository/TransactionRepository';
-import TransactionRepositoryDatabase from '../../../src/infra/repository/TransactionRepositoryDatabase';
-import SQLDataBaseGateway from '../../../src/infra/gateway/SQLDataBaseGateway';
-import SQLDataBaseGatewayPGP from '../../../src/infra/gateway/SQLDataBaseGatewayPGP';
 import crypto from 'crypto';
+import ProcessPayment from '../../../../src/app/usecase/ProcessPayment';
+import PaymentGateway from '../../../../src/infra/gateway/PaymentGateway';
+import PaymentGatewayRede from '../../../../src/infra/gateway/PaymentGatewayRede';
+import TransactionRepository from '../../../../src/infra/repository/TransactionRepository';
+import TransactionRepositoryDatabase from '../../../../src/infra/repository/TransactionRepositoryDatabase';
+import SQLDataBaseGateway from '../../../../../account/src/infra/gateway/SQLDataBaseGateway';
+import SQLDataBaseGatewayPGP from '../../../../../account/src/infra/gateway/SQLDataBaseGatewayPGP';
 
 describe('testes para caso de uso de ProcessPayment', () => {
   let database: SQLDataBaseGateway;
@@ -49,7 +49,7 @@ describe('testes para caso de uso de ProcessPayment', () => {
     const input = paymentData;
     const output = await usecase.execute(input);
     await expect(output).toBeUndefined();
-    const transaction = await transactionRepository.getTransactionByRideId(paymentData.rideId);
+    const transaction = await transactionRepository.getRideTransaction(paymentData.rideId);
     expect(transaction).toBeDefined();
   });
 });
