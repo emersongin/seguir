3e9b18c3a09bd8db801dc88b362ee02535fb7df9
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-03-12 15:34:00 -0300

    test: adicionado teste para caso de uso signupAsync

---
+++ backend/account/test/integration/api/signupAsync.test.ts
@@ -0,0 +1,32 @@
+import crypto from 'crypto';
+import Queue from '../../../src/infra/queue/Queue';
+import RabbitMqAdapter from '../../../src/infra/queue/RabbitMqAdapter';
+
+describe('teste para enfileramento de nova de conta', () => {
+  let queue: Queue;
+
+  beforeAll(async () => {
+    queue = new RabbitMqAdapter();
+    await queue.connect();
+  });
+
+  afterAll(async () => {
+    await queue.close();
+  });
+
+  it('deve enfilerar uma nova de conta', async () => {
+    const uuid = crypto.randomUUID();
+    const input = {
+      name: 'Emerson Andrey',
+      email: `test_${uuid}@hotmail.com`,
+      password: 'senha_valida',
+      cpf: '649.731.080-06',
+      isDriver: false,
+      isPassenger: true,
+      carPlate: null,
+      creditCardToken: '123456789'
+    };
+    const output = await queue.publish('signup', input);
+    expect(output).toBe(true);
+  });
+});
