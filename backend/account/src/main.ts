19186d11ac3c7f14a0dcdb776367a62f3c48828e
Emerson Andrey <emersonandrey_7@hotmail.com>
2024-03-13 07:26:45 -0300

    feat: separação de http account de queue

--- backend/account/src/main.ts
+++ backend/account/src/main.ts
@@ -4,13 +4,9 @@ import PgPromiseAdapter from './infra/database/PgPromiseAdapter';
 import AccountRepositoryDatabase from './infra/repository/AccountRepositoryDatabase';
 import Registry from './infra/di/Registry';
 import httpController from './infra/http/httpController';
-import QueueController from './infra/queue/QueueController';
 import ExpressAdapter from './infra/http/ExpressAdapter';
-import RabbitMqAdapter from './infra/queue/RabbitMqAdapter';
 
 async function run() {
-  const queue = new RabbitMqAdapter();
-  await queue.connect();
   const pgpDatabase = new PgPromiseAdapter();
   await pgpDatabase.connect();
   const accountRepository = new AccountRepositoryDatabase(pgpDatabase);
@@ -21,7 +17,6 @@ async function run() {
   registry.register('getAccount', getAccount);
   const server = new ExpressAdapter();
   const controller = new httpController(server);
-  const queueController = new QueueController(queue);
   const port = 3000;
   server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- backend/account/src/queue.ts
+++ backend/account/src/queue.ts
@@ -1,21 +1,10 @@
-import amqp from 'amqplib';
+import QueueController from './infra/queue/QueueController';
+import RabbitMqAdapter from './infra/queue/RabbitMqAdapter';
 
-async function main() {
-  try {
-    const connection = await amqp.connect({
-      hostname: 'localhost',
-    });
-    if (connection.connection) {
-      console.log('Conexão estabelecida com o RabbitMQ.');
-    } else {
-        console.log('A conexão com o RabbitMQ não foi estabelecida.');
-    }
-    const channel = await connection.createChannel();
-    channel.assertQueue('queue', { durable: true });
-    channel.sendToQueue('queue', Buffer.from('{ "name": "Emerson Andrey" }'));
-  } catch (error) {
-    console.error('Error:', error);
-  }
+async function run() {
+  const queue = new RabbitMqAdapter();
+  await queue.connect();
+  const queueController = new QueueController(queue);
 }
 
-main();
+run();
